68624e373c3bd0ebcebf3e38d6c66998c9d4d9b9
Doreen <doreenlulu97@gmail.com>
2022-12-07 01:55:50 +0300

    Getting pizzas

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -3,6 +3,6 @@ class RestaurantPizzasController < ApplicationController
 
     private
     def render_unprocessable_entity_response(invalid)
-        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+        render json: { errors: invalid.record.errors.full_messages }, status: :render_unprocessable_entity_response
     end
 end
